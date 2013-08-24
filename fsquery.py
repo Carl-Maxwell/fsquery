44c6afafce38737835b63acefb2f70c962a57165
Carl Maxwell <carl.maxwell@gmail.com>
2013-08-23 23:56:53 -0700

    using fnmatch to allow the use of the wildcard operator in queries, also using import from in the hope that it improves performance (need to do more testing to confirm)

--- fsquery.py
+++ fsquery.py
@@ -1,5 +1,6 @@
-import os
+from os import walk, path
 import argparse
+from fnmatch import fnmatch
 
 parser = argparse.ArgumentParser(description='Query your filesystem to find a file')
 
@@ -10,17 +11,40 @@ parser.add_argument(
 	nargs='+'
 )
 
+parser.add_argument(
+	'--first',
+	dest="first",
+	action="store_const",
+	const=True,
+	default=False,
+	help='only find the first result'
+)
+
 args = parser.parse_args()
 
-def query(start_path, terms):
-	for path, directories, files in os.walk(start_path):
-		if len(terms) == 1 and terms[0] in files:
-			print os.path.join(path, terms[0])
-		elif terms[0] in directories:
-			if len(terms) > 1:
-				query(os.path.join(path, terms[0]), terms[1:])
-			elif terms[0] in directories:
-				print os.path.join(path, terms[0])
-				
-query('./', args.terms)
+is_dir = args.terms[-1][-1] == '/'
 
+if is_dir:
+	args.terms[-1] = args.terms[-1][0:-1]
+
+def match(term, l):
+	for e in l:
+		if fnmatch(e, term):
+			return e
+	return False
+
+def query(start_path, terms):
+	for current_path, directories, files in walk(start_path):
+		if len(terms) == 1:
+			if is_dir:
+				thing = match(terms[0], directories)
+			else:
+				thing = match(terms[0], files)
+			if thing:
+				print path.join(current_path, terms[0])
+				if args.first:
+					exit()
+		elif len(terms) > 1 and match(terms[0], directories):
+			query(path.join(current_path, terms[0]), terms[1:])
+
+query('./', args.terms)
